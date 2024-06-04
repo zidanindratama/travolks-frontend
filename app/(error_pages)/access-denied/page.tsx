00683ffbfc261c2d8c292305cf9f67c8e6d22bd6
zidanindratama <zidanindratama31@gmail.com>
2024-06-04 11:05:46 +0700

    add: coomig soon page, access denied page, and not found page

---
+++ app/(error_pages)/access-denied/page.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center m-auto space-y-6">
+      <div className="font-semibold text-center text-4xl capitalize">
+        Access Denied
+      </div>
+      <Image
+        src={"/gif/access-denied.gif"}
+        alt="access denied"
+        width={300}
+        height={300}
+      />
+      <Link href="/">
+        <Button variant={"primaryBlue"}>Back to Home Page</Button>
+      </Link>
+    </div>
+  );
+};
+
+export default page;
